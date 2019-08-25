e23d40343e5bc0c31f5b101b136f7a79e9a3150d
Javone Smith <javosmith12@gmail.com>
2019-08-25 15:21:05 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,7 +1,7 @@
 def turn(board)
   puts "Please enter 1-9"
   input = gets.chomp #input is 4
-  index = input_to_index(input) #index is 3, refenecing input number index starts at 0
+  index = input_to_index(input) #index is 3, referencing input number index starts at 0
   if valid_move?(board, index)
     #we want to show user move on board
     move(board, index)
